de280edf1ae84ff501752e81754c4b1f634019ee
Kevin Villegas <kevin.villegas94@gmail.com>
2022-07-06 08:07:27 -0700

    added download link, game pictures and background images/styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import "./App.css";
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
+        <div className="App">
+            <h1>Welcome to Naruto Street Fighter</h1>
+            <h3>
+                Fight against the enemy in different naruto themed
+                battlegrounds!
+            </h3>
+            <a href="https://github.com/KevinVillegasDev/Naruto-Street-Fighter/archive/refs/tags/v1.zip">
+                Download game file here
+            </a>
+            <h2>Main Menu</h2>
+            <img src={process.env.PUBLIC_URL + "/mainmenu.png"} />
+            <h2>Gameplay</h2>
+            <img src={process.env.PUBLIC_URL + "/game.png"} />
         </div>
     );
 };
